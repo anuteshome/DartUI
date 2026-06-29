f79d921420ce75989e22f550d9e1e779babe4241
@Nu73424545 <ananyateshome2@gmail.com>
2026-06-29 23:50:23 +0300

    fix: Search bar

---
+++ lib/Component/ShoeTile.dart
@@ -0,0 +1,15 @@
+import "package:dartui/Models/ShoesModel.dart";
+import "package:flutter/material.dart";
+
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+   ShoeTile({super.key,required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Models/ShoesModel.dart
@@ -0,0 +1,13 @@
+class Shoe {
+  final String name;
+  final String ImagePath;
+  final String Discription;
+  final String Price;
+
+  Shoe({
+    required this.name,
+    required this.ImagePath,
+    required this.Discription,
+    required this.Price,
+  });
+}
