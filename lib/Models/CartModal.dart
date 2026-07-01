3cfdb7c08f3e438544cc178ed39f43aa07dd309f
@Nu73424545 <ananyateshome2@gmail.com>
2026-07-01 15:16:01 +0300

    fix: Create Cart modal

---
+++ lib/Models/CartModal.dart
@@ -0,0 +1,50 @@
+import "package:dartui/Models/ShoesModel.dart";
+
+class Cart {
+  List<Shoe> ShopShoe = [
+    Shoe(
+      name: "Running Shoe",
+      Price: "120",
+      Discription:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      ImagePath: "lib/Assets/Images/shoes3.png",
+    ),
+    Shoe(
+      name: "Waking Shoe",
+      Price: "150",
+      Discription:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      ImagePath: "lib/Assets/Images/shors4.png",
+    ),
+    Shoe(
+      name: "Sport Shoe",
+      Price: "300",
+      Discription:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      ImagePath: "lib/Assets/Images/shoes5.png",
+    ),
+    Shoe(
+      name: "Coding Shoe",
+      Price: "100",
+      Discription:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      ImagePath: "lib/Assets/Images/shoes2.png",
+    ),
+  ];
+  List<Shoe> UserCart = [];
+
+  List<Shoe> getUserCart() {
+    return UserCart;
+  }
+
+  List<Shoe> getShopShoe() {
+    return ShopShoe;
+  }
+
+  void AddShoe(Shoe shoe) {
+    UserCart.add(shoe);
+  }
+void RemoveShoe(Shoe shoe){
+    UserCart.remove(shoe);
+  }
+}
