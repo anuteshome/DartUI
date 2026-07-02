3779defb12fb2a97c90fbb79d986c05c699f1c32
@Nu73424545 <ananyateshome2@gmail.com>
2026-07-02 16:31:56 +0300

    fix: Adding and setup delete button for a cart

--- lib/Component/CartTile.dart
+++ lib/Component/CartTile.dart
@@ -1,6 +1,7 @@
 import "package:dartui/Models/ShoesModel.dart";
 import "package:flutter/material.dart";
 import "package:dartui/Models/CartModal.dart";
+import "package:provider/provider.dart";
 
 class CartTile extends StatefulWidget {
   Shoe shoe;
@@ -16,18 +17,21 @@ class _CartTileState extends State<CartTile> {
     return Padding(
       padding: const EdgeInsets.all(25.0),
       child: Container(
-      height:100,
-      decoration:BoxDecoration(
-        color:Colors.grey[100],
-        borderRadius:BorderRadius.circular(12)
-      ),
-      child:ListTile(
-        leading:Image.asset(widget.shoe.ImagePath),
-        title:Text(widget.shoe.name),
-        subtitle:Text(widget.shoe.Price.toString()),
-      )
-      
-      
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: ListTile(
+          leading: Image.asset(widget.shoe.ImagePath),
+          title: Text(widget.shoe.name),
+          subtitle: Text(widget.shoe.Price.toString()),
+          trailing: IconButton(
+            onPressed: () {
+              Provider.of<Cart>(context, listen: false).RemoveShoe(widget.shoe);
+            }, icon: Icon(Icons.delete),
+          ),
+        ),
       ),
     );
   }
